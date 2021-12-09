d89bb18d4cdfbd354f27f7a9ec5dc51e7589a755
Adowalma <adowalmaadao@gmail.com>
2021-12-09 15:58:07 +0100

    upload do project

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,28 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    tarefas:[
+      {id:1, titulo:"Ir ao mercado", concluido:false},
+      {id:2, titulo:"Ir ao cinema", concluido:false},
+    ]
   },
   mutations: {
+    adicionarTarefa(state,titulo){
+      if(titulo){
+        state.tarefas.push(
+          { 
+            id: new Date().getTime(),
+            titulo,
+            concluido:false
+          }
+        )
+      }
+    },
+    removeTarefa(state,id){
+      //filter e exatamente para filtar algun dado de um array
+      state.tarefas = state.tarefas.filter(tarefa => tarefa.id != id)
+    }
+
   },
   actions: {
   },
