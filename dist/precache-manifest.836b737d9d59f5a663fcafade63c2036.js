0cf3a0346ff766118cee5afa3843f0c68198729a
Adowalma <adowalmaadao@gmail.com>
2021-12-09 16:01:51 +0100

    upload Dist

---
+++ dist/precache-manifest.836b737d9d59f5a663fcafade63c2036.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15c9f86708d3f594b81f",
+    "url": "/css/chunk-17bc2355.c479b06d.css"
+  },
+  {
+    "revision": "906025172ca426e901de",
+    "url": "/css/chunk-vendors.2973f4f6.css"
+  },
+  {
+    "revision": "564f9b0258d5b92fd649c21dbcb10cec",
+    "url": "/img/bg.564f9b02.jpg"
+  },
+  {
+    "revision": "284787691f76bd16dd7cbaf735be162f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06f29a4daacb2bf4b8ff",
+    "url": "/js/app.74ece954.js"
+  },
+  {
+    "revision": "15c9f86708d3f594b81f",
+    "url": "/js/chunk-17bc2355.79176f4f.js"
+  },
+  {
+    "revision": "f252a965c2a3a4787170",
+    "url": "/js/chunk-2d22d746.4805bb82.js"
+  },
+  {
+    "revision": "906025172ca426e901de",
+    "url": "/js/chunk-vendors.6060ac2f.js"
+  },
+  {
+    "revision": "9ba57f9b60e6bd57955b45c89fcd7b66",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
